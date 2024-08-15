9436dff687debdb267d564afd2be8fcc733ecb93
connorholcombe <124104172+connorholcombe@users.noreply.github.com>
2024-08-15 09:16:48 -0400

    Add files via upload

---
+++ jfm/topBorder.js
@@ -0,0 +1,13 @@
+const topBorder = document.getElementById('top-bar');
+const initialOffsetTop = topBorder.offsetTop;
+window.addEventListener('scroll', () => {
+    if (window.scrollY > initialOffsetTop) {
+        topBorder.style.position = 'absolute';
+        topBorder.style.top = '0';
+        topBorder.style.left = `-${window.scrollX}px`;
+    } else {
+        topBorder.style.position = 'fixed';
+        topBorder.style.top = `${initialOffsetTop}px`;
+        topBorder.style.left = '0';
+    }
+});
