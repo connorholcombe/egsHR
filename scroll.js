40c4740aacb7d982c3d64f1aa460673b4c0a66d0
connorholcombe <124104172+connorholcombe@users.noreply.github.com>
2024-08-29 08:21:45 -0400

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,29 @@
+function scrollContact() {
+const section = document.getElementById("scrollContact");
+section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+    duration: 500
+});
+}
+
+function scrollAbout() {
+const section = document.getElementById("scrollAbout");
+section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+    duration: 500
+});
+}
+
+function scrollTop() {
+const section = document.getElementById("scrollTop");
+section.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+    duration: 500
+});
+}
